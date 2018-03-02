b6c7df6ed6dccc13d7f26f71cb01078fa30d7a14
PrismClimbing <36991539+PrismClimbing@users.noreply.github.com>
2018-03-02 14:38:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+$('.nav-link a').on('click',function(e) {
+    if($("collapse container").hasClass('in')) {
+        e.stopPropagation();
+    }
+    // You can also add preventDefault to remove the anchor behavior that makes
+    // the page jump
+    // e.preventDefault();
+});
