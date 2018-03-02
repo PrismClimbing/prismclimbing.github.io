022c89099433895618a495e6b6bc0341ba0a4724
PrismClimbing <36991539+PrismClimbing@users.noreply.github.com>
2018-03-02 15:18:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ $('.collapse.container').on('hidden.bs.collapse', function (e) {
   }
 });
 $(".nav-link").on("click", function(){
-  $(".nav-link").find(".active").removeClass("active");
+  $("active nav-link").removeClass("active");
   $(this).addClass("active");
 });
