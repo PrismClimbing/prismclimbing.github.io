5d732c5287e1c6c39f675f84d5627b3954d91c5f
PrismClimbing <36991539+PrismClimbing@users.noreply.github.com>
2018-03-12 17:36:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $('.collapse.container').on('hidden.bs.collapse', function (e) {
   if($('.collapse.container').length !== 2) {
 	$("#" + e.currentTarget.id.toString()).collapse('show');
+	$(".active.nav-link").removeClass("active");
+        $("#" + $("." + e.currentTarget.id.toString()).aria-labelledby).addClass("active");
   }
-});
-$(".nav-link").on("click", function(){
-  $(".active.nav-link").removeClass("active");
-  $(this).addClass("active");
 });
