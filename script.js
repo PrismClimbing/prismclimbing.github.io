c9ff49e951cdbab8c8cc2af3b2f119ec5b2e1102
PrismClimbing <PrismClimbing@gmail.com>
2018-03-14 18:57:00 +0100

    Fix brandname click to redirect to home

--- script.js
+++ script.js
@@ -41,9 +41,15 @@ $('.navbar-brand').click(e => {
 
 $('.collapse.no-transition').on('shown.bs.collapse', (e) => {
   if (e.currentTarget !== $('.active.nav-link')[0]) {
-    $('.active.nav-link').removeClass('active');
-    $('#' + $('#' + e.currentTarget.id).attr('aria-labelledby')).addClass('active');
-    window.location.hash = $('.active.nav-link').attr('href');
+    if (e.currentTarget.getAttribute('aria-labelledby') == 'openHome2') {
+      $('.active.nav-link').removeClass('active');
+      $('#openHome').addClass('active');
+      window.location.hash = 'home';
+    } else {
+      $('.active.nav-link').removeClass('active');
+      $('#' + $('#' + e.currentTarget.id).attr('aria-labelledby')).addClass('active');
+      window.location.hash = $('.active.nav-link').attr('href');
+    }
   }
 });
 
